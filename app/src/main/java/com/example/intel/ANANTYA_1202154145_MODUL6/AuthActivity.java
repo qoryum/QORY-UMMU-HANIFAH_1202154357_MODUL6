23c35e04e6b2d9524b6ca20126fe6a72f0d50a04
Anank1ng <35679021+Anank1ng@users.noreply.github.com>
2018-04-01 22:05:02 +0700

    auth Clear

---
+++ app/src/main/java/com/example/intel/ANANTYA_1202154145_MODUL6/AuthActivity.java
@@ -0,0 +1,101 @@
+package com.example.intel.ANANTYA_1202154145_MODUL6;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AuthActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
+    EditText loginEmail, loginPassword;
+    Button btnLogin;
+    TextView linkSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        // create new firebase instance
+        auth = FirebaseAuth.getInstance();
+
+        // check if user already logged in
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(AuthActivity.this, MainActivity.class));
+            finish();
+        }
+
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPassword = findViewById(R.id.loginPassword);
+        linkSignUp = findViewById(R.id.linkSignUp);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        // to sign up activity
+        linkSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AuthActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginFirebase();
+            }
+        });
+    }
+
+    private void loginFirebase() {
+        String email = loginEmail.getText().toString();
+        final String password = loginPassword.getText().toString();
+
+        // checking fields
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Your email is still empty",
+                    Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Your password is still empty",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        // login user
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(AuthActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // if login not success
+                        if (!task.isSuccessful()) {
+                            if (password.length() < 6) { // if password less than 6 chars
+                                loginPassword.setError(getString(R.string.minimum_password));
+                            } else {
+                                Toast.makeText(AuthActivity.this,
+                                        "Authentication Failed!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        // if login success
+                        else {
+                            Intent intent = new Intent(AuthActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/intel/ANANTYA_1202154145_MODUL6/Post.java
@@ -0,0 +1,53 @@
+package com.example.intel.ANANTYA_1202154145_MODUL6;
+
+/**
+ * Created by Yuda on 31/03/2018.
+ */
+
+public class Post {
+    String postId;
+    private String username;
+    private String photo;
+    private String photoTitle;
+    private String photoDesc;
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getPhotoTitle() {
+        return photoTitle;
+    }
+
+    public void setPhotoTitle(String photoTitle) {
+        this.photoTitle = photoTitle;
+    }
+
+    public String getPhotoDesc() {
+        return photoDesc;
+    }
+
+    public void setPhotoDesc(String photoDesc) {
+        this.photoDesc = photoDesc;
+    }
+}
